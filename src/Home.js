5520d95c7c3b942dedb588dbe40ed7c6d88b4851
rherugu <rherugu@gmail.com>
2020-04-10 11:50:08 -0400

    frontend

--- src/Home.js
+++ src/Home.js
@@ -1,93 +1,396 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './Home.css';
+import React, { useState } from "react";
+import "./Home.css";
+import "./Home.scss";
 import Component from "@reactions/component";
-import Coves_Bold from './coves-typeface/Coves_Bold.otf';
+import TextLoop from "react-text-loop";
+import BackgroundSlideshow from "react-background-slideshow";
+import "./fonts.css";
+import ReactPlayer from "react-player";
+import $ from "jquery";
 
-import 'react-tabs/style/react-tabs.css';
-import "react-awesome-button/dist/styles.css";
-import { Button, Divider, Form, Grid, Segment } from 'semantic-ui-react'
-
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
-import Shop from './Shop';
-
-
-class Home extends Component{
-
-onClickHome = () =>{
-  this.props.history.push("/")
-}
-onClickShop = () =>{
-  this.props.history.push("/Shop")
-}
-onClickAbout = () =>{
-  this.props.history.push("/About")
-}
-onClickContact = () =>{
-  this.props.history.push("/Contact")
-}
-onClickLogin = () =>{
-  this.props.history.push("/login")
-}
-
-
-constructor(props) {
-  super(props);
-  this.state = {
-    fname: '',
-    lname: '',
-    email: '',
-    bname: '',
-    description: '',
-    Address: '',
-    mailSent: false,
-    error: null,
-    PhoneNumber: '',
-    logins: false,
+class Home extends Component {
+  componentDidMount() {
+    this.title();
   }
-}
+  title = () => {
+    document.title = "LocalMainStreet";
+  };
 
-handleFormSubmit( event ) {
-  event.preventDefault();
-  console.log(this.state);
-}
+  onClickHome = () => {
+    this.props.history.push("/");
+  };
+  onClickShop = () => {
+    this.props.history.push("/Shop");
+  };
+  onClickAbout = () => {
+    this.props.history.push("/About");
+  };
+  onClickContact = () => {
+    this.props.history.push("/Contact");
+  };
+  onClickLogin = () => {
+    this.props.history.push("/login");
+  };
 
-render(){
-  return (
-    <div className="Home">
-      <header className="Home-header">
-        <div className="logoimg">
-        <img src={require('./Assets/LOGObg.png')}></img>
+  onClickCustomerLogin = () => {
+    this.props.history.push("/CustomerLogin");
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      fname: "",
+      lname: "",
+      email: "",
+      bname: "",
+      description: "",
+      Address: "",
+      mailSent: false,
+      error: null,
+      PhoneNumber: "",
+      logins: false,
+    };
+  }
+
+  handleFormSubmit(event) {
+    event.preventDefault();
+    console.log(this.state);
+  }
+
+  render() {
+    // $(window).scroll(function() {
+    //   if (
+    //     $(window).scrollTop() + $(window).height() >
+    //     $(document).height() - 100
+    //   ) {
+    //     this.document.getElementById("wrapper").style.width = "80%";
+    //     this.document.getElementById("wrapper").style.margin = "auto";
+    //     this.document.getElementById("header").style.width = "80%";
+    //   } else {
+    //     this.document.getElementById("wrapper").style.width = "100%";
+    //     this.document.getElementById("wrapper").style.height = "100%";
+    //   }
+    // });
+
+    return (
+      <div className="slideshowbg">
+        <div className="Home">
+          <title>LocalMainStreet</title>
+
+          <div
+            className="Home-headerP"
+            id="header"
+            style={{ backgroundColor: "#ffffff" }}
+          >
+            <div className="Home-header">
+              <div className="logoimg" onClick={this.onClickHome}>
+                <img
+                  src={require("./Assets/golo.png")}
+                  className="logoimage"
+                ></img>
+              </div>
+
+              <div className="tabs">
+                <div className="navtabs" onClick={this.onClickHome}>
+                  <span id="btnspan">Home</span>
+                </div>
+                <br></br>
+                <div className="navtabs" onClick={this.onClickShop}>
+                  <span id="btnspan">Shop</span>
+                </div>
+                <br></br>
+                <div className="navtabs" onClick={this.onClickAbout}>
+                  <span id="btnspan">About</span>
+                </div>
+                <br></br>
+                <div className="navtabs" onClick={this.onClickContact}>
+                  <span id="btnspan">Contact</span>
+                </div>
+                <br></br>
+                <div className="divider"></div>
+
+                <div
+                  className="navtabsL"
+                  id=".dropbtn"
+                  onClick={this.onClickLogin}
+                >
+                  <span id="btnspan">Login</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <div className="main">
+            <big>
+              <big>
+                <big>
+                  <big>
+                    <big>
+                      <big>
+                        <big>
+                          <big>
+                            <h1 className="textloop">
+                              <TextLoop
+                                interval={1500}
+                                springConfig={{ stiffness: 180, damping: 8 }}
+                              >
+                                <span className="ICG">Inspire.</span>
+                                <span className="ICG">Create.</span>
+                                <span className="ICG">
+                                  &nbsp;&nbsp;Grow.&nbsp;&nbsp;
+                                </span>
+                              </TextLoop>{" "}
+                            </h1>
+                          </big>
+                        </big>
+                      </big>
+                    </big>
+                  </big>
+                </big>
+              </big>
+            </big>
+            <button style={{ visibility: "hidden" }}>
+              <div
+                onClick={this.onClickCustomerLogin}
+                className="butn btn-four"
+                style={{ visibility: "visible" }}
+              >
+                <span className="span" style={{ fontSize: 32 }}>
+                  Get Started
+                </span>
+              </div>
+            </button>
+          </div>
         </div>
-        <div className="tabs">
+        <div className="bgcover">
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <big style={{ textAlign: "center" }}>
+            <big>
+              <big>
+                <big>
+                  <big>
+                    <h1 className="WWA">What We Do</h1>
+                  </big>
+                </big>
+              </big>
+            </big>
+          </big>
+          <br></br>
 
-        <button class="btn" onClick= { this.onClickHome }
-        ><span>Home</span></button>
-        <br></br>
-        <button class="btn" onClick= { this.onClickShop }><span>Shop</span></button>
-        <br></br>
-        <button class="btn" onClick= { this.onClickAbout }><span>About</span></button>
-        <br></br>
-        <button class="btn" onClick= { this.onClickContact }><span>Contact</span></button>
-        <br></br>
-        <button class="btn" id=".dropbtn" onClick= { this.onClickLogin }><span>Login</span></button>
-  
+          <div className="parentvid">
+            <div className="videop">
+              <div
+                className="video"
+                style={{
+                  textAlign: "center",
+                }}
+              >
+                <iframe
+                  width="560"
+                  height="315"
+                  src="https://www.youtube.com/embed/h3dKMLCbOhk"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen
+                  className="video"
+                ></iframe>
+              </div>
+            </div>
+
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <big>
+              <h2 className="textvid">
+                During the Covid 19 outbreak, we connect local businesses to
+                you. Since they are making no money, you can help by buying gift
+                cards and redeeming them later.{" "}
+              </h2>
+            </big>
+          </div>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+
+          <big>
+            <big>
+              <big>
+                <h1 className="Available">Available For Download</h1>
+              </big>
+            </big>
+          </big>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <img className="phoneimg" src={require("./Assets/phone.png")}></img>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <div className="home-footer">
+            <div className="logoandterms">
+              <div className="logoimg" onClick={this.onClickHome}>
+                <img
+                  src={require("./Assets/golo.png")}
+                  className="logoimage"
+                  style={{ width: "450px" }}
+                ></img>
+              </div>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <a className="terms">Terms and Conditions</a>
+              <a className="terms1"> | </a>
+              <a className="terms">Privacy Policy</a>
+            </div>
+
+            <div className="screen">
+              <div className="screen-header">
+                <div className="screen-header-left">
+                  <div className="screen-header-button close"></div>
+                  <div className="screen-header-button maximize"></div>
+                  <div className="screen-header-button minimize"></div>
+                </div>
+                <div className="screen-header-right">
+                  <div className="screen-header-ellipsis"></div>
+                  <div class="screen-header-ellipsis"></div>
+                  <div className="screen-header-ellipsis"></div>
+                </div>
+              </div>
+              <div className="screen-body">
+                <div className="screen-body-item left">
+                  <div className="app-title">
+                    <span>CONTACT</span>
+                    <span>US</span>
+                  </div>
+                  <div className="app-contact">
+                    CONTACT INFO : +1 732 803 8584
+                  </div>
+                </div>
+                <div className="screen-body-item">
+                  <div className="app-form">
+                    <div className="app-form-group">
+                      <input
+                        className="app-form-control"
+                        placeholder="NAME"
+                      ></input>
+                    </div>
+                    <div className="app-form-group">
+                      <input
+                        className="app-form-control"
+                        placeholder="EMAIL"
+                      ></input>
+                    </div>
+                    <div className="app-form-group message">
+                      <textarea
+                        className="app-form-control"
+                        placeholder="MESSAGE"
+                        rows="6"
+                        columns="10"
+                      ></textarea>
+                    </div>
+                    <div className="app-form-group buttons">
+                      <button className="app-form-button" type="reset">
+                        CANCEL
+                      </button>
+                      <button className="app-form-button">SEND</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </header>
-      <div className="bgcover">
-        <br></br>
-        
       </div>
-    </div>
-    
-
-  );
-}
+    );
+  }
 }
 
 export default Home;
--- src/Shop.js
+++ src/Shop.js
@@ -2,6 +2,10 @@ import React from "react";
 import "./Shop.css";
 import axios from "axios";
 import Component from "@reactions/component";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import StripeCheckout from "react-stripe-checkout";
+import $ from "jquery";
 
 class Shop extends Component {
   onClickHome = () => {
@@ -19,6 +23,15 @@ class Shop extends Component {
   onClickLogin = () => {
     this.props.history.push("/login");
   };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      shops: [],
+      price: 25
+    };
+  }
+
   componentDidMount() {
     const tokenval = localStorage.getItem("token");
     const headers = {
@@ -28,40 +41,102 @@ class Shop extends Component {
       .get("http://localhost:3000/shop", { headers })
       .then(response => {
         console.log(response.data);
+
+        this.setState({ shops: response.data });
       })
       .catch(function(err) {
         alert(err);
       });
   }
+
   render() {
+    async function handleToken(token) {
+      const product = {
+        name: "SmartTicketing",
+        price: 25
+      };
+      const response = await axios.post("http://localhost:8080/", {
+        token,
+        product
+      });
+      const { status } = response.data;
+      if (status === "success") {
+        alert("Success! Check email for details");
+      } else {
+        alert("Something went wrong.");
+      }
+    }
+
     return (
       <div className="Shop">
-        <header className="Shop-header">
-          <div className="logoimgS">
-            <img src={require("./Assets/LOGObg.png")}></img>
+        <div className="Shop-headerP" style={{ backgroundColor: "#ffffff" }}>
+          <div className="Shop-header">
+            <div className="logoimg" onClick={this.onClickHome}>
+              <img
+                src={require("./Assets/golo.png")}
+                className="logoimage"
+              ></img>
+            </div>
+
+            <div className="tabs">
+              <div className="navtabs" onClick={this.onClickHome}>
+                <span id="btnspan">Home</span>
+              </div>
+              <br></br>
+              <div className="navtabs" onClick={this.onClickShop}>
+                <span id="btnspan">Shop</span>
+              </div>
+              <br></br>
+              <div className="navtabs" onClick={this.onClickAbout}>
+                <span id="btnspan">About</span>
+              </div>
+              <br></br>
+              <div className="navtabs" onClick={this.onClickContact}>
+                <span id="btnspan">Contact</span>
+              </div>
+              <br></br>
+              <div className="divider"></div>
+
+              <div
+                className="navtabsL"
+                id=".dropbtn"
+                onClick={this.onClickLogin}
+              >
+                <span id="btnspan">Login</span>
+              </div>
+            </div>
           </div>
-          <div className="tabs">
-            <button class="btn" onClick={this.onClickHome}>
-              <span>Home</span>
-            </button>
-            <br></br>
-            <button class="btn" onClick={this.onClickShop}>
-              <span>Shop</span>
-            </button>
-            <br></br>
-            <button class="btn" onClick={this.onClickAbout}>
-              <span>About</span>
-            </button>
-            <br></br>
-            <button class="btn" onClick={this.onClickContact}>
-              <span>Contact</span>
-            </button>
-            <br></br>
-            <button class="btn" id=".dropbtn" onClick={this.onClickLogin}>
-              <span>Login</span>
-            </button>
-          </div>
-        </header>
+        </div>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+
+        <StripeCheckout
+          stripeKey="pk_test_KkfXWjgjLwtNgUTOjtn25pj4005QCLSJ6I"
+          token={handleToken}
+          billingAddress
+          shippingAddress
+          amount={this.state.price * 100}
+        ></StripeCheckout>
+
+        <div class="searchBox">
+          <input
+            class="searchInput"
+            type="text"
+            name=""
+            placeholder="Search"
+          ></input>
+          <button class="searchButton" href="#">
+            <i class="material-icons">search</i>
+          </button>
+        </div>
       </div>
     );
   }
